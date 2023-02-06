7e26f2889a545e7c8601f8ea9abfc3e0edd6aae2
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2023-02-06 08:41:43 -0500

    Initial setup of simple Angular app that will display a random selection of NYTimes recipes. Styling is terrible atm.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { URLList } from './constants';
+
+interface Link {
+  URL: string;
+  Title: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'random-nytimes-recipe';
+  public displayRecipeList: boolean = false;
+  public recipeList: Link[] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    console.log(URLList[Math.floor(Math.random() * URLList.length)]);
+  }
+
+  public generateRandomRecipeList() {
+    this.recipeList = [];
+    for (let i = 0; i < 5; i++) {
+      let randomURL = this.getRandomRecipe();
+      let randomRecipe: Link = {
+        URL: randomURL,
+        Title: this.getRecipeTitleFromURL(randomURL),
+      };
+      this.recipeList.push(randomRecipe);
+    }
+  }
+
+  public goToRandomRecipe() {
+    window.location.href = this.getRandomRecipe();
+  }
+
+  private getRecipeTitleFromURL(url: string): string {
+    let title = url.replace('https://cooking.nytimes.com/recipes/', '');
+    title = title.replace(/^\d+/g, '');
+    title = this.toTitleCase(title);
+    return title;
+  }
+
+  private toTitleCase(oldString: string): string {
+    let newString = oldString
+      .split('-')
+      .map(function (word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(' ');
+
+    return newString;
+  }
+
+  private getRandomRecipe() {
+    return URLList[Math.floor(Math.random() * URLList.length)];
+  }
 }
